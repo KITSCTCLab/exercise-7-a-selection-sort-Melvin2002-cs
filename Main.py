c6086da5c2f2620d03d6ef7077cedebcc3538720
Melvin2002-cs <110010657+Melvin2002-cs@users.noreply.github.com>
2022-09-13 11:25:21 +0530

    1st

--- Main.py
+++ Main.py
@@ -1,7 +1,26 @@
 from typing import List
 
+def swap(arr[int j],arr[int k]):
+  temp=arr[int j]
+  arr[int j]=arr[int k]
+  arr[int k]=temp
+
 def selectionSort(array, size) -> List[int]:
   # Write your code here
+  if size==1:
+    return array
+  execution=0
+  for i in range(0,size-1):
+    low=i
+    for j in range(i+1,size):
+      if array[low]>array[j]:
+        low=j
+        execution=1
+    if execution!=0:
+      swap(array[i],array[low])
+  
+  
+  
 
 # Do not change the following code
 input_data = input()
